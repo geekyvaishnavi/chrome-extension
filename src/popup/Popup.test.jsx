35d9cc88e05530e001dfa55fad55f616be64d4ae
Vaishnavi <gaikwadvaishnavi1000@gmail.com>
2026-04-19 17:30:10 +0530

    chore: remove tests, mocks, and testing setup

--- src/popup/Popup.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Popup from "./Popup";
-
-test("renders todo heading", () => {
-  render(<Popup />);
-  expect(screen.getByText(/todo/i)).toBeInTheDocument();
-});
--- src/popup/hooks/useTasks.test.js
+++
@@ -1,7 +0,0 @@
-import { renderHook } from "@testing-library/react";
-import { useTasks } from "./useTasks";
-
-test("should initialize with empty tasks", () => {
-  const { result } = renderHook(() => useTasks());
-  expect(result.current.tasks).toEqual([]);
-});
